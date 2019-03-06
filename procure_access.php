7ee047dbfa2bb3fbf1adf77a14b618a4add219b2
Ayobami Babalola <babalolaisaac@gmail.com>
2019-03-06 10:03:48 +0100

    Update procure_access.php

--- procure_access.php
+++ procure_access.php
@@ -13,3 +13,10 @@ Vendor
 Host:procure.lfcww.org
 user:procure
 password:Lrs5OiNW6hloE8M
+  
+  
+  //API Auth
+  
+  host:pauth_dbx
+  username:dbxadmin@pauth.lfcww.org
+  password:7632gybd2bd2g782dg682whuew9hu
